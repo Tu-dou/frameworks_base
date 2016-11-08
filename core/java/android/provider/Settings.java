f3aa5bf5e4e1c264eedfc5afd1b9edc5f5e8c6e4
Tu-dou <3108451290@qq.com>
2016-11-08 21:42:08 +0800

    fix build omniswitch
    Signed-off-by: Tu-dou <3108451290@qq.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4284,6 +4284,12 @@ public final class Settings {
          public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
 
         /**		
+         * Whether to allow fullscreen screenshots or let the user select which area to screenshot
+         * @hide
+         */
+        public static final String SCREENSHOT_TYPE = "screenshot_type";
+
+        /**		
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
